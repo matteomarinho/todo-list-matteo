c9b4499e2fe6273be9a6d03441b809802f0cbc40
matteomarinho <matteo.marinho@estiam.com>
2023-04-19 07:28:30 +0200

    new code

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,49 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [selectedTodo, setSelectedTodo] = useState(null);
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [priority, setPriority] = useState('');
 
-  const handleAddTodo = (event) => {
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks) {
+      setTasks(storedTasks);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const todo = event.target.elements.todo.value;
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), text: todo }]);
-      event.target.reset();
+    if (newTask.trim()) {
+      const task = {
+        id: Date.now(),
+        name: newTask,
+        priority,
+      };
+      setTasks([...tasks, task]);
+      setNewTask('');
+      setPriority('');
     }
   };
 
+  const handleDelete = (id) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
+  };
 
-  const handleDeleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-    if (selectedTodo && selectedTodo.id === id) {
-      setSelectedTodo(null);
-    }
+  const handleEdit = (id, name, priority) => {
+    const editedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, name, priority } : task
+    );
+    setTasks(editedTasks);
   };
 
   return (
-    <div>
-      <h1>To-Do List</h1>
-      <form onSubmit={handleAddTodo}>
-        <input type="text" name="todo" placeholder="Add a to-do" />
-        <button type="submit">Add</button>
+    <div className="todo-container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Task name:
+          <input
+            type="text"
+            value={newTask}
+            onChange={(event) => setNewTask(event.target.value)}
+          />
+        </label>
+        <label>
+          Priority:
+          <select value={priority} onChange={(event) => setPriority(event.target.value)}>
+            <option value="">Select priority</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </label>
+        <button type="submit">Add task</button>
       </form>
       <ul>
-  {todos.map((todo) => (
-    <li
-      key={todo.id}
-      className={selectedTodo && selectedTodo.id === todo.id ? 'selected' : ''}
-      onClick={() => setSelectedTodo(todo)} 
-      >
-        
-      {todo.text}
-      <button onClick={() => handleDeleteTodo(todo.id)}>remove</button>
-    </li>
-  ))}
-</ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.name} ({task.priority})
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
+            <button onClick={() => handleEdit(task.id, prompt('Edit task name:', task.name), prompt('Edit priority:', task.priority))}>Edit</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default TodoList;
 
